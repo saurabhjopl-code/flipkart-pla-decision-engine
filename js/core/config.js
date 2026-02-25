158a58ba93e289766e0a2b1bce70cfb70ef13126
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-25 14:57:10 +0530

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,1 +1,6 @@
-// Configuration file
+// js/core/config.js
+
+export const CONFIG = {
+  TRAFFIC_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSlTVpLNaHcWlBzhl0Tho9cSXOD80l3ma_c_Fa-P7IoY7R22X1ewihnY1zZOCHwduVyTUOhyKmRnGkT/pub?gid=1438975073&single=true&output=csv",
+  SALES_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSlTVpLNaHcWlBzhl0Tho9cSXOD80l3ma_c_Fa-P7IoY7R22X1ewihnY1zZOCHwduVyTUOhyKmRnGkT/pub?gid=706667677&single=true&output=csv"
+};
